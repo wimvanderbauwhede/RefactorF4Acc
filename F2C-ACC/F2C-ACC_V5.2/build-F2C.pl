5f253fab3736a74fd9710de54bf5f9eec4ad96b0
Wim Vanderbauwhede <Wim.Vanderbauwhede@gmail.com>
2013-11-28 13:34:58 +0000

    - build script and executable now use $CC, $FC rather than template-based hardcoding - added -m64 flag to nvcc opts, so it's 64-bit only

--- F2C-ACC/F2C-ACC_V5.2/build-F2C.pl
+++ F2C-ACC/F2C-ACC_V5.2/build-F2C.pl
@@ -10,18 +10,17 @@ What I did was change ifort to gfortran, icc to gcc, and hardcoded the path to n
 I also created a separate gptl_lite_m32 because libcuda on (my) Mac is 32-bit
 
 =cut
-
-my $F2ACC_DOC_VERSION = 'V4.2';
-my $CUDA_LIB_PATH='/usr/local/cuda/lib/';
 # build script to make and install the F2C compiler
 #
-# Written by:   Mark Govett
+# Original csh script written by:   Mark Govett
 #               NOAA Earth System Research Laboratory
 #               June 2013
+# 
+# This Perl script: Wim Vanderbauwhede
 #
 # Required software
-#   - intel Fortran and C compiler:             test:  which ifort
-#   - nvcc compiler                             test:  which nvcc
+#   - Fortran and C compiler, define through $FC and $CC
+#   - nvcc compiler                             test:  which nvcc, set $CUDA_ROOT
 #   - cuda runtime library                      test:  find   libcudart.so
 #   - gnu m4 library                            test:  which m4
 #
@@ -35,23 +34,33 @@ my $CUDA_LIB_PATH='/usr/local/cuda/lib/';
 #   2.  Directory where you wish to install F2C-ACC exists
 #
 #
-use Getopt::Std;
 use Cwd;
+
+my $cwd=cwd();
+
+my $F2ACC_DOC_VERSION = 'V4.2';
+my $CUDA_LIB_PATH=$ENV{'CUDA_ROOT'}.'/lib/';
+
+my $F2C_HOME=$ENV{'F2C_HOME'};
+my $F2C_ROOT=$cwd;
+$ENV{'F2C_HOME'}=$F2C_ROOT;
+use Getopt::Std;
 use File::Copy;
 my %opts=();
-getopts('hvcti:', \%opts);
+getopts('hvCcti:', \%opts);
+my $CA= $opts{'C'} // 0;
 my $C= $opts{'c'} // 0;
 my $V= $opts{'v'} // 0;
 my $T= $opts{'t'} // 0;
 my $I= $opts{'i'} ? 1 : 0;
 my $installdir = $opts{'i'};
-my $cwd=cwd();
 
 if ($opts{'h'}) { 
   say "Usage: $0 [-c -v -i] install-path";
   say "  install-path: location where F2C-ACC will be installed";
   say "  -i: Install";
-  say "  -c: Clean before building";
+  say "  -C: Clean both F2C-ACC source and examples before building";
+  say "  -c: Clean examples before building";
   say "  -t: Build and run examples for testing";
   say "  -v: Verbose";
   exit 0;
@@ -63,60 +72,69 @@ if ($I) {
         exit 1;
     } else {
 # remove anything in there! danger mouse!
-#        system("rm -Rf $installdir/*");
+        system("rm -Rf $installdir/bin/*");
+        system("rm -Rf $installdir/examples/*");
+        system("rm -Rf $installdir/lib/*");
+        system("rm -Rf $installdir/include/*");
     }
 }
 my $mydir = $cwd;
 #
 # build F2C-ACC compiler;
 chdir 'source';
+system('make clean') if $CA;
 my $res1=`make`;
 if (not $res1) {
     say 'First make failed--exiting'; 
     exit 1;
+} else {
+    print $res1 if $V;
 }
 move 'F2C-ACC.exe','../bin';
 chdir '..';
-#
-# modify perl driver script F2C-ACC, to build	
-#
-say 'chdir bin' if $V;
-chdir 'bin/';
-#copy 'F2C-ACC.template', 'F2C-ACC';
-#sed -i s,/Users/name/,$mydir, F2C-ACC ;
-open my $TEMPL, '<','F2C-ACC.template';
-open my $OUT, '>','F2C-ACC';
-while (my $line = <$TEMPL>) {
-    $line=~s/\/Users\/name\//$mydir/g;
-    print $OUT $line;
-}
-close $TEMPL;
-close $OUT;
-chmod 0755, 'F2C-ACC';
-chdir '..';
-if ($T) {
-# Building examples for testing
-say 'chdir examples/' if $V;
-chdir 'examples/';
-#copy 'Makefile.template', 'Makefile';
-#sed -i s,/Users/name/,$mydir, Makefile;
-open $TEMPL, '<','Makefile.template';
-open $OUT, '>','Makefile';
-while (my $line = <$TEMPL>) {
-    $line=~s/\/Users\/name\//$mydir/g;
-    print $OUT $line;
-}
-close $TEMPL;
-close $OUT;
-system('make clean') if $C;
-#
-chdir '../lib';
-system('make clean') if $C;
+# #
+# # modify perl driver script F2C-ACC, to build	
+# #
+# say 'chdir bin' if $V;
+# chdir 'bin/';
+# #copy 'F2C-ACC.template', 'F2C-ACC';
+# #sed -i s,/Users/name/,$mydir, F2C-ACC ;
+# open my $TEMPL, '<','F2C-ACC.template';
+# open my $OUT, '>','F2C-ACC';
+# while (my $line = <$TEMPL>) {
+#     $line=~s/\/Users\/name\//$mydir/g;
+#     print $OUT $line;
+# }
+# close $TEMPL;
+# close $OUT;
+# chmod 0755, 'F2C-ACC';
+# chdir '..';
+
+
+chdir 'lib';
+system('make clean') if $CA;
 my $res2 =`make`;
 if (not $res2) {
     say 'Second make failed--exiting';
     exit 1;
 }
+chdir '..';
+
+ if ($T) {
+# # Building examples for testing
+ say 'chdir examples/' if $V;
+ chdir 'examples/';
+# #copy 'Makefile.template', 'Makefile';
+# #sed -i s,/Users/name/,$mydir, Makefile;
+# open $TEMPL, '<','Makefile.template';
+# open $OUT, '>','Makefile';
+# while (my $line = <$TEMPL>) {
+#     $line=~s/\/Users\/name\//$mydir/g;
+#     print $OUT $line;
+# }
+# close $TEMPL;
+# close $OUT;
+system('make clean') if $C;
 #
 say "Building gptl_lite...";
 chdir '../examples/gptl_lite';
@@ -139,10 +157,12 @@ say '';
 say '---------------------------------------';
 say 'Validating F2C-ACC Generated C code';
 say '---------------------------------------';
+# on Mac OS X 10.8.5 with gfortran-mp-4.8, it needs DYLD_LIBRARY_PATH=/opt/local/lib/gcc48 or /opt/local/lib/libgcc for libgfortran.3.dylib
 system('./driver_cpu');
 say '---------------------------------------';
 say 'Validating F2C-ACC Generated CUDA code';
 say '---------------------------------------';
+# on Mac OS X 10.8.5 with gfortran-mp-4.8, it needs DYLD_LIBRARY_PATH=<path to 32-bit libgfortran.3.dylib>
 system("LD_LIBRARY_PATH=$CUDA_LIB_PATH ./driver_gpu");
 chdir '..';
 }
@@ -153,32 +173,32 @@ if ($I) {
     if ( ($cwd ne $installdir) && ($cwd.'/' ne $installdir)) {
 
 # modify paths to point to the install path;
-    chdir 'bin' ;
+#    chdir 'bin' ;
 #  unlink 'F2C-ACC';
 #  copy('F2C-ACC.template','F2C-ACC');
 #  sed -i s,/Users/name/,$installdir, F2C-ACC ;
-    open $TEMPL, '<','F2C-ACC.template';
-    open $OUT, '>','F2C-ACC_for_install';
-    while (my $line = <$TEMPL>) {
-        $line=~s/\/Users\/name\//$installdir/g;
-        print $OUT $line;
-    }
-    close $TEMPL;
-    close $OUT;
+#    open $TEMPL, '<','F2C-ACC.template';
+#    open $OUT, '>','F2C-ACC_for_install';
+#    while (my $line = <$TEMPL>) {
+#        $line=~s/\/Users\/name\//$installdir/g;
+#        print $OUT $line;
+#    }
+#    close $TEMPL;
+#    close $OUT;
 
 # Adapt Makefile  for tests to build in $instaldir
-    chdir '../examples/';
+#    chdir '../examples/';
 #  copy -f Makefile.template Makefile;
 #  sed -i s,/Users/name/,$installdir, Makefile;
-    open $TEMPL, '<','Makefile.template';
-    open $OUT, '>','Makefile';
-    while (my $line = <$TEMPL>) {
-        $line=~s/\/Users\/name\//$installdir/g;
-        print $OUT $line;
-    }
-    close $TEMPL;
-    close $OUT;
-    chdir '..';
+#    open $TEMPL, '<','Makefile.template';
+#    open $OUT, '>','Makefile';
+#    while (my $line = <$TEMPL>) {
+#        $line=~s/\/Users\/name\//$installdir/g;
+#        print $OUT $line;
+#    }
+#    close $TEMPL;
+#    close $OUT;
+#    chdir '..';
 
 # install F2C in the user specified location;
     say "Copying documentation to $installdir ..." if $V; 
@@ -195,7 +215,7 @@ if ($I) {
     if (! -e "$installdir/bin") {
         mkdir "$installdir/bin";
     }
-    copy 'bin/F2C-ACC_for_install', "$installdir/bin/F2C-ACC";
+    copy 'bin/F2C-ACC', "$installdir/bin/F2C-ACC";
     chmod 0755, "$installdir/bin/F2C-ACC";
     copy 'bin/F2C-ACC.exe', "$installdir/bin";
 # Copy examples for tests to $installdir
