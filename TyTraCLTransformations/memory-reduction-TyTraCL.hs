81d7d0c7ef7609e3fb79ffc8eb63220e6f502729
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2018-11-23 17:23:52 +0000

    Rewrite rules for memory reduction on TyTraCL. Most of the code is written, but the rules need to be applied until a fixed point is reached.

--- TyTraCLTransformations/memory-reduction-TyTraCL.hs
+++ TyTraCLTransformations/memory-reduction-TyTraCL.hs
@@ -11,14 +11,21 @@ main = do
     mapM print ast'
     putStrLn "\nSubstitute vectors (recursive)"
     mapM print ast''
+    putStrLn "\nApply rewrite rules"
+    mapM print ast'''
+{-    
     putStrLn "\nTest for Vec in RHS Expr"
     mapM (print . get_vec_subexprs . snd) ast''
     putStrLn "\nTest for non-input Vec in RHS Expr"
     mapM (print . expr_has_non_input_vecs . snd) ast'
     putStrLn "\nTest for non-input Vec in RHS Expr"
     mapM (print . expr_has_non_input_vecs . snd) ast''
+    -- Tests for bottom-up reduction using everywhere
+    print ll
+    print (reduce_ll ll)
+    print (reduce_tree tree)
 --    print $ find_in_ast ast' (Vec VT "duu_1")
-
+-}
 {-
     main (etan_0,wet_0,eps_0,dt_0,g_0,dx_0,dy_0,wet_0,u_0,v_0,duu_0,dvv_0,h_0,dt_0,dx_0,dy_0,hzero_0,hmin_0) =
       let
@@ -56,7 +63,8 @@ type AST = [(Expr,Expr)]
 
 data Expr =
         -- Left-hand side:
-                      Scalar 
+                      Scalar Name
+                    | Const Int  
                     | Tuple [Expr]
                     | Vec VE Name
 
@@ -70,8 +78,9 @@ data Expr =
                     | Fold Expr Expr Expr
                     | Stencil Expr Expr 
                     | Function Name 
-                    | ApplyT [Name]  
-                    | MapS Name
+                    | ApplyT [Expr]  
+                    | MapS Expr
+                    | Comp Expr Expr
                         deriving (Show, Typeable, Data, Eq)
 
         -- Initially, all RHS nodes will be Map, Fold or Stencil.
@@ -180,9 +189,20 @@ Finally, we go trough all lines of the ast and do a recursive substitution:
 -}    
 ast'' =  map (substitute_vec_rec ast') (filter lhs_is_output_vec ast')
 
+{-
+The AST is now reduced to a list of tuples where the first elt (LHS) is an output vector and the second element is an expression which only contains input vectors.     
+Now we should start applying the rewrite rules to reduce each of these expressions to a single Map.
+-}
+
+
+
+
+
+
 lhs_is_output_vec (lhs_vec,expr) = case lhs_vec of
     Vec VO _ -> True
     _ -> False
+
 -- Create a list of all Vecs in the expr
 -- Find the corresponding RHS expressions
 -- Substitute all of them in the given expression
@@ -206,3 +226,92 @@ substitute_vec_rec ast expr_tup@(lhs_vec,expr) = let
                 substitute_vec_rec ast expr_tup'
         else -- we're dome, return the result 
             expr_tup
+
+-- A linked list
+ll = Tuple [ Const 11, Tuple [ Const 22, Tuple [ Const 33, Tuple [ Const 44, Tuple [   Const 55, Const 66 ] ] ] ] ]
+
+
+-- Sum all nodes in the linked list
+reduce_ll ll = everywhere (mkT reduce_ll_chain) ll
+
+reduce_ll_chain expr = case expr of
+    Tuple [ Const n, Const m] -> Const (n+m)
+    _ -> expr
+
+
+{-
+ A binary tree
+                    11
+                /       \
+             22            44    
+           /   \        /     \
+          33    0      55      66
+         / \          /  \    /  \
+        0   0        0    0  0    0  
+-}
+tree = Tuple [ Const 11, Tuple [ Const 22, Tuple [ Const 33, Const 0, Const 0], Const 0], Tuple [ Const 44, Tuple [   Const 55, Const 0, Const 0], Tuple [Const 66, Const 0, Const 0 ] ] ] 
+
+-- Sum all nodes in the binary tree
+reduce_tree tree = everywhere (mkT reduce_subtree) tree
+
+reduce_subtree expr = case expr of
+    Tuple [ Const n, Const m, Const k] -> Const (n+m+k)
+    _ -> expr
+
+    
+-- With this preparation I guess we are ready for the actual rewrites:
+--
+rewrite_ast_into_single_map ast = everywhere (mkT rewrite_ast_sub_expr) ast
+
+{-
+Rewrite rules
+1.
+    map f_2 (map f_1 v) = map (f_2 . f_1) v    
+2.
+    v = map id v -- but only if this v appears in a ZipT where at least one of the other expressions is a map
+3.
+    zipt (map f_1 v_1, map f_2 v_2) = map (applyt (f_1,f_2)) (zipt (v_1, v_2)) -- if all exprs in ZipT are maps
+4.
+    applyt (g_1,g_2) $ applyt (f_1,f_2)  = applyt (g_1 . f_1, g_2 . f_2)
+5.
+    (elt i) . unzipt . (map f (zipt (...,v_i,...))) = map ((elt i) . f) (zipt (...,v_i,...))
+6.
+    stencil s_1 (map f_1) = map (maps f_1) (stencil s_1)
+-}
+
+rewrite_ast_sub_expr expr = case expr of 
+    -- 1.
+    Map f1_expr (Map f2_expr v_expr) -> Map (Comp f1_expr f2_expr) v_expr
+    -- 6.
+    Stencil s_1 (Map f_1 v_expr) -> Map (MapS f_1) (Stencil s_1 v_expr)   
+    ZipT es -> 
+        -- If all args of ZipT are Map
+        if (length ( filter isMap es ) == length es) 
+            -- 4.
+            then rewriteZipTMap es
+            -- Otherwise, check if at least one of them is a map, if so, rewrite Vec to Id
+            else if (length ( filter isMap es ) > 0) 
+                -- 3.
+                then ZipT (map rewriteId es) 
+                else expr
+    -- 5.                
+    Elt i_expr (UnzipT (Map f_expr (ZipT es))) -> Map (Comp (PElt i_expr) f_expr) (ZipT es)
+    _ -> expr
+
+ast''' = map (\(lhs,rhs) -> (lhs,rewrite_ast_into_single_map rhs)) ast''
+
+isMap expr = case expr of
+    Map _ _ -> True
+    _ -> False
+
+-- (map f_1 v_1, map f_2 v_2) = map (applyt (f_1,f_2)) (zipt (v_1, v_2))    
+rewriteZipTMap es =  let
+        f_s = map (\(Map f v) -> f) es
+        v_s = map (\(Map f v) -> v) es
+    in
+        Map (ApplyT f_s) (ZipT v_s)
+
+rewriteId expr =  case expr of
+    Vec vt n -> Map (Function "id") expr
+    _ -> expr
+
