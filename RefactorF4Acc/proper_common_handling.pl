4d8ab27b3f3db915f45ad5ae7bb687f6b3950834
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2019-05-28 21:08:19 +0100

    nm

--- RefactorF4Acc/proper_common_handling.pl
+++ RefactorF4Acc/proper_common_handling.pl
@@ -17,8 +17,6 @@ if (not $commonsInIncludes) {
     }
 }
 
-#my %common_blocks=();
-
 
 for my $common_decl_str_ ('common v1,v2, /BLK1/ v7// v3, v4, / BLK2 / v5,v6', 'common w1,w2',
     'common / blk3/ u,uu(9)//z(3)'
@@ -27,14 +25,11 @@ for my $common_decl_str_ ('common v1,v2, /BLK1/ v7// v3, v4, / BLK2 / v5,v6', 'c
 $stref = _collect_common_vars_per_block($stref, $f, $common_decl_str_);    
 
 }
-# This is done after all COMMON blocks have been parsed, so say for simplicity at the end of parsing of a code unit, or alternatively when the first non-decl statement is encountered.
-#
-$stref = _create_common_block_var_sequences($stref, $f);
-# Now we can parse all common var decl strings
-# And we can also get the dimensions 
-# I think I will keep the old code to handle the declarations
 
+     say "\nCOMMON BLOCKS:\n";
+    say Dumper($stref->{'Subroutines'}{$f}{'CommonBlocks'});
 
+# Now we can create the pairs with dimensions in Analysis
 
 
 sub _match_up_common_vars { my ($stref,$f) = @_;
@@ -42,7 +37,8 @@ sub _match_up_common_vars { my ($stref,$f) = @_;
    return $stref;
 }
 
-
+# This is for Parser, so the result should a list of var names
+#
 sub _collect_common_vars_per_block { my ($stref, $f, $common_decl_str_) = @_;
     my %common_blocks = %{ $stref->{'Subroutines'}{$f}{'CommonBlocks'} };
     my $common_decl_str = $common_decl_str_;
@@ -54,30 +50,24 @@ sub _collect_common_vars_per_block { my ($stref, $f, $common_decl_str_) = @_;
     }
     my  @common_chunks = split(/\s*\/\s*/,$common_decl_str);
 
-    #say Dumper(@common_chunks);
     while (@common_chunks) {
+
         my $common_block_name = shift @common_chunks;
+        if ( $common_block_name eq '' or $common_block_name=~/^\s+$/) {  $common_block_name = 'BLANK';}
+
+        if (not exists $common_blocks{$common_block_name}) {
+            $common_blocks{$common_block_name}=[];
+        }
         my $common_vars_str = shift @common_chunks;
+
         $common_vars_str=~s/,\s*$//;
-        if ( $common_block_name=~/^\s*$/) {  $common_block_name = 'BLANK';}
-        push @{$common_blocks{$common_block_name}} ,$common_vars_str;
+        my @common_vars_strs = split(/\s*,\s*/,$common_vars_str);
+        my @common_vars =  map { s/\(.+$// ;$_} @common_vars_strs ;
+                        $common_blocks{$common_block_name} =[ @{$common_blocks{$common_block_name}} ,@common_vars ];
     }
-
     $stref->{'Subroutines'}{$f}{'CommonBlocks'}={  %common_blocks };
     return $stref;
 }
 
 
-sub _create_common_block_var_sequences { my ($stref, $f, $common_decl_str_) = @_;
-    my %common_blocks = %{ $stref->{'Subroutines'}{$f}{'CommonBlocks'} };
-    for my $common_block_name (keys %common_blocks) {
-        $common_blocks{$common_block_name}=[split(/\s*,\s*/,join(',',@{$common_blocks{$common_block_name}}))];
-    }
-    # Now parse them and create the pairs with the dimensions
-
-    say Dumper(%common_blocks);
-
-    $stref->{'Subroutines'}{$f}{'CommonBlocks'}={  %common_blocks };
-    return $stref;
-}
 
