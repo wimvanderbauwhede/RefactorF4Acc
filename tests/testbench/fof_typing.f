74d15b3966798b2576d9bcd8f08764c45526c493
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2020-08-20 17:13:28 +0100

    nm

--- tests/testbench/fof_typing.f
+++ tests/testbench/fof_typing.f
@@ -1,16 +1,42 @@
       program fof
-          external  sqsum
-          external  f2
-          integer  z
-          !call ten_times(sqsum,3,4,z,.false.)
-          call ten_times(f2,3,4,z,.false.)
+          !integer sqsum
+          external sqsum
+          external f2
+          integer x,y,z
+          x=3
+          y=4
+          !z = sqsum(x,y)
+          !print *,z            
+          !call f2(x,y,z)
+          !print *,z            
+          call ten_times_f(sqsum,3,4,z,.false.)
+          print *,z
+          call ten_times_s(f2,3,4,z,.false.)
           print *,z
           
       end 
       
-      subroutine ten_times(f3,x,y,z,c)
-          !integer  f3 ! for a function
-          external  f3 ! for a subroutine but not needed
+      subroutine ten_times_f(f3,x,y,z,c)
+          integer  f3 ! for a function
+          integer x,y
+          integer z
+          real r1,r2
+          logical c
+          r1=7.1
+          r2=8.8
+          if (c) then
+          z = f3(x,y)
+          else
+          ! patently wrong:
+          ! logical -> real -> real
+          print *, 'Wrong!'
+          z = f3(c,r1,r2,x,y) ! 
+          end if
+          !print *,z
+      end 
+
+      subroutine ten_times_s(f3,x,y,z,c)
+          !external f3 ! for a subroutine but not needed
           integer x,y
           integer z
           real r1,r2
@@ -28,8 +54,7 @@
           !z = f3(c,r1,r2,x,y) ! 
           end if
           !print *,z
-      end 
-      
+      end       
       subroutine f2(x,y,z)
           integer x,y
           integer z
