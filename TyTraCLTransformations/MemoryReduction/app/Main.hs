320a2f37d8919f03337d2a7257fa0156e05c6ff7
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2021-05-24 16:39:23 +0100

    Change from compiled module to AST as input file

--- TyTraCLTransformations/MemoryReduction/app/Main.hs
+++ TyTraCLTransformations/MemoryReduction/app/Main.hs
@@ -1,20 +1,23 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {- HLINT "ignore Use when" -}
 module Main where
+import System.Environment    
 import Control.Monad ( when )
 import TyTraCLAST 
-import ASTInstance (ast,functionSignaturesList)
+-- import ASTInstance (ast,functionSignaturesList)
 import Transforms (splitLhsTuples, substituteVectors, applyRewriteRules, fuseStencils, decomposeExpressions)
 import CodeGeneration (
     inferSignatures, 
     generateFortranCode
     )
+import ASTReader ( readAST )
 
 info 
     | noStencilRewrites = True
     | otherwise = True
 
 data Stage = Original | SplitLhsTuples | SubstituteVectors | ApplyRewriteRules | FuseStencils | DecomposeExpressions deriving (Show, Ord, Eq)
+{-
 stage 
     | noStencilRewrites = DecomposeExpressions
     | otherwise = DecomposeExpressions
@@ -37,12 +40,73 @@ asts  -- = ast4
     | stage == FuseStencils = [ast3']
     | stage == DecomposeExpressions = ast4
 
+transformedASTs ast stage 
+        | stage == Original = [ast]
+        | stage == SplitLhsTuples = [ast1]
+        | stage == SubstituteVectors = [ast2]
+        | stage == ApplyRewriteRules = [ast3]
+        | stage == FuseStencils = [ast3']
+        | stage == DecomposeExpressions = ast4
+    where
+        ast1 :: TyTraCLAST
+        ast1 = splitLhsTuples ast
+        ast2 :: TyTraCLAST
+        ast2 = substituteVectors ast1
+        (ast3 :: TyTraCLAST, (_,idSigList)) = applyRewriteRules ast2
+        ast3' :: TyTraCLAST
+        ast3' = fuseStencils ast3
+        ast4 :: [TyTraCLAST]
+        ast4 = decomposeExpressions ast1 ast3' 
+
 inferedSignatures :: [[(Name,FSig)]]
 inferedSignatures = map inferSignatures ast4
 
 generatedFortranCode = generateFortranCode asts functionSignaturesList idSigList 
+-}
 
 main = do
+    astFilePathLst <- getArgs
+    let
+        astFilePath
+            | null astFilePathLst = error "Please specify the path to the ASTInstance file"
+            | otherwise = head astFilePathLst
+    astInst <- readAST astFilePath -- better make this the file name provided as argument
+    let
+        (   ast, 
+            functionSignaturesList, 
+            stencilDefinitionsList, 
+            mainArgDeclsList, 
+            scalarisedArgsList, 
+            origNamesList
+            ) = astInst
+    -- print astInst
+    let
+        stage 
+            | noStencilRewrites = DecomposeExpressions
+            | otherwise = DecomposeExpressions
+        
+        ast1 :: TyTraCLAST
+        ast1 = splitLhsTuples ast
+        ast2 :: TyTraCLAST
+        ast2 = substituteVectors ast1
+        (ast3 :: TyTraCLAST, (_,idSigList)) = applyRewriteRules ast2
+        ast3' :: TyTraCLAST
+        ast3' = fuseStencils ast3
+        ast4 :: [TyTraCLAST]
+        ast4 = decomposeExpressions ast1 ast3' 
+        
+        asts  -- = ast4
+            | stage == Original = [ast]
+            | stage == SplitLhsTuples = [ast1]
+            | stage == SubstituteVectors = [ast2]
+            | stage == ApplyRewriteRules = [ast3]
+            | stage == FuseStencils = [ast3']
+            | stage == DecomposeExpressions = ast4
+        inferedSignatures :: [[(Name,FSig)]]
+        inferedSignatures = map inferSignatures ast4
+        -- We must pass astInst as arg into this instead of functionSignaturesList
+        generatedFortranCode = generateFortranCode asts functionSignaturesList idSigList 
+                
     if info 
         then
             do
--- TyTraCLTransformations/MemoryReduction/src/ASTReader.hs
+++ TyTraCLTransformations/MemoryReduction/src/ASTReader.hs
@@ -1,125 +1,17 @@
-{-# LANGUAGE DeriveDataTypeable #-}
-module Main where
+module ASTReader where
 
+import TyTraCLAST
 
-import Data.Generics (Data, Typeable)
-import Data.List (intercalate)
-
-type Name = String
-type FType = String
-type Size = Int
--- Vector In   Out  Stencil Temp Don't care
-data VE = VI  | VO  | VS  | VT | VDC deriving (Show, Read, Ord, Typeable, Data, Eq)
-data DType = 
-    DInteger | DInt 
-  | DReal | DFloat 
-  | DSVec Int DType --  to encode SVecs
-  | DTuple [DType] -- to encode Tuple
-  | DDC -- Don't Care ; Int and Integer, Real and Float as I can't make up my mind
-    deriving (Show, Read, Ord, Typeable, Data, Eq)
-
-type FSig = [Expr]
-
-data FName = Single String | Composite [FName]
-  deriving (Ord, Typeable, Data, Eq)
-
-instance Show FName where    
-  show (Single nm) = nm
-  show (Composite nms) = 
-    intercalate ", " (map show nms)
-
-data FIntent = In | Out | InOut | Unknown | NA
-  deriving (Show, Read, Ord, Typeable, Data, Eq)
-
-data FDecl = MkFDecl {
-  ftype :: String,  
-  dim :: Maybe [Int],
-  intent :: Maybe FIntent,
-  names :: [String] 
-  } |
---  deriving (Ord, Typeable, Data, Eq)
- MkFParamDecl {
-  ftype :: String,  
-  dim :: Maybe [Int],
-  name :: String,
-  val :: String 
-  }
- deriving (Ord, Read, Typeable, Data, Eq)
-
-errorDecl = MkFDecl "ERROR" Nothing Nothing []
-
-instance Show FDecl where  
-  show (MkFDecl ftype mdim intent names ) = let
-        attributes = [ftype]
-        attributes' = case mdim of          
-          Just dims -> let
-              dims_str = intercalate "," $ map (\dm ->  "1:"++(show dm)) dims
-            in
-              attributes++["dimension("++dims_str++")"]
-          Nothing -> attributes
-        attributes'' = case intent of
-            Just i -> attributes'++["intent("++(show i)++")"]
-            Nothing -> attributes'
-      in
-        (intercalate ", " attributes'') ++ " :: "++ (intercalate ", " names)
-  show (MkFParamDecl ftype mdim name val) = let
-    maybe_dimstr = case mdim of          
-      Just dims -> let
-          dims_str = intercalate "," $ map (\dm ->  "1:"++(show dm)) dims
-        in
-          ", dimension("++dims_str++")"
-      Nothing -> ""
-    in
-      ftype++", parameter"++maybe_dimstr++" :: "++ name ++ " = " ++ val
-        
-
-type TyTraCLAST = [(Expr,Expr)]                      
-type FunctionSignature = [(String,[Expr])]      
-type StencilDefinition = [(String,[Integer])]    
-type MainArgDecls = [(String,FDecl)]
-type ScalarisedArgs = [(String,[(String,(Integer,FIntent,String))])]
-type OrigNames = [(String,[(String,[(String,FIntent)])])]
-
-type ASTInstance = (TyTraCLAST, FunctionSignature, StencilDefinition, MainArgDecls, ScalarisedArgs, OrigNames) 
-
-data Expr =
-        -- Left-hand side:
-                      Scalar VE DType Name
-                    | Const Int
-                    | Tuple [Expr]
-                    | Vec VE Expr -- Name -- (Vec ve (SVec sz (Scalar ve dt v_n) ) )
-
-        -- Right-hand side:
-                    | SVec Size Expr -- Name
-                    | ZipT [Expr]
-                    | UnzipT Expr
-                    | Elt Int Expr
-                    | PElt Int -- partially applied Elt
-                    | Map Expr Expr -- map f v
-                    | Fold Expr Expr Expr -- fold f acc v
-                    | Stencil Expr Expr -- stencil s v
-                    | Function Name [Expr] -- 2nd arg is list of non-map/fold args
-                    | Id Name [Expr] -- id, but it's treated as a named function for ease of code generation
-                    | ApplyT [Expr]  -- applyt (f1,f2)
-                    | MapS Expr Expr -- maps s f
-                    | Comp Expr Expr -- comp f2 f1
-                    | FComp Expr Expr -- like comp but to combine a fold and a map, quite a-hoc!
-                    | SComb Expr Expr -- scomb s1 s2
-                        deriving (Show, Read, Ord, Typeable, Data, Eq)
-
-main :: IO ()
-main = do
-  ast <- readAST "ast-to-read.hs"
-  print ast
+-- main :: IO ()
+-- main = do
+--   astFilePath <- getArgs
+--   ast <- readAST astFilePath -- better make this the file name provided as argument
+--   print ast
 
 readAST :: FilePath -> IO ASTInstance
 readAST path = do
-  alldata <- readFile path
+  ast_str <- readFile path
   let
-    -- ast :: ASTInstance
-     ast = read alldata
+     ast = read ast_str
   return ast
-  -- return alldata
-
-
 
