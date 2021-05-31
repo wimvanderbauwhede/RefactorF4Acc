b4ef39ccc34941d6a6ffc3a3b152d358343cdd20
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2021-05-31 09:29:45 +0100

    Reverted changes for reading in AST at run time, postpone for now.

--- TyTraCLTransformations/MemoryReduction/app/Main.hs
+++ TyTraCLTransformations/MemoryReduction/app/Main.hs
@@ -1,23 +1,20 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {- HLINT "ignore Use when" -}
 module Main where
-import System.Environment    
 import Control.Monad ( when )
 import TyTraCLAST 
--- import ASTInstance (ast,functionSignaturesList)
+import ASTInstance (ast,functionSignaturesList)
 import Transforms (splitLhsTuples, substituteVectors, applyRewriteRules, fuseStencils, decomposeExpressions)
 import CodeGeneration (
     inferSignatures, 
     generateFortranCode
     )
-import ASTReader ( readAST )
 
 info 
     | noStencilRewrites = True
     | otherwise = True
 
 data Stage = Original | SplitLhsTuples | SubstituteVectors | ApplyRewriteRules | FuseStencils | DecomposeExpressions deriving (Show, Ord, Eq)
-{-
 stage 
     | noStencilRewrites = DecomposeExpressions
     | otherwise = DecomposeExpressions
@@ -40,77 +37,12 @@ asts  -- = ast4
     | stage == FuseStencils = [ast3']
     | stage == DecomposeExpressions = ast4
 
-transformedASTs ast stage 
-        | stage == Original = [ast]
-        | stage == SplitLhsTuples = [ast1]
-        | stage == SubstituteVectors = [ast2]
-        | stage == ApplyRewriteRules = [ast3]
-        | stage == FuseStencils = [ast3']
-        | stage == DecomposeExpressions = ast4
-    where
-        ast1 :: TyTraCLAST
-        ast1 = splitLhsTuples ast
-        ast2 :: TyTraCLAST
-        ast2 = substituteVectors ast1
-        (ast3 :: TyTraCLAST, (_,idSigList)) = applyRewriteRules ast2
-        ast3' :: TyTraCLAST
-        ast3' = fuseStencils ast3
-        ast4 :: [TyTraCLAST]
-        ast4 = decomposeExpressions ast1 ast3' 
-
 inferedSignatures :: [[(Name,FSig)]]
 inferedSignatures = map inferSignatures ast4
 
 generatedFortranCode = generateFortranCode asts functionSignaturesList idSigList 
--}
 
 main = do
-    astFilePathLst <- getArgs
-    let
-        astFilePath
-            | null astFilePathLst = error "Please specify the path to the ASTInstance file"
-            | otherwise = head astFilePathLst
-    astInst <- readAST astFilePath -- better make this the file name provided as argument
-    let
-        (   ast, 
-            functionSignaturesList, 
-            stencilDefinitionsList, 
-            mainArgDeclsList, 
-            scalarisedArgsList, 
-            origNamesList
-            ) = astInst
-    -- print astInst
-    let
-        stage 
-            | noStencilRewrites = DecomposeExpressions
-            | otherwise = DecomposeExpressions
-        
-        ast1 :: TyTraCLAST
-        ast1 = splitLhsTuples ast
-        ast2 :: TyTraCLAST
-        ast2 = substituteVectors ast1
-        (ast3 :: TyTraCLAST, (_,idSigList)) = applyRewriteRules ast2
-        ast3' :: TyTraCLAST
-        ast3' = fuseStencils ast3
-        ast4 :: [TyTraCLAST]
-        ast4 = decomposeExpressions ast1 ast3' 
-        
-        asts  -- = ast4
-            | stage == Original = [ast]
-            | stage == SplitLhsTuples = [ast1]
-            | stage == SubstituteVectors = [ast2]
-            | stage == ApplyRewriteRules = [ast3]
-            | stage == FuseStencils = [ast3']
-            | stage == DecomposeExpressions = ast4
-        functionSignatures :: Map.Map Name FSig
-        functionSignatures =  Map.fromList functionSignaturesList
-            
-        inferedSignatures :: [[(Name,FSig)]]
-        inferedSignatures = map (inferSignatures functionSignatures) ast4
-        -- We must pass astInst as arg into this instead of functionSignaturesList
-        -- generatedFortranCode = generateFortranCode asts functionSignaturesList idSigList 
-        generatedFortranCode = generateFortranCode asts astInst idSigList 
-                
     if info 
         then
             do
