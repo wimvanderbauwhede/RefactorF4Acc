b6e5b0b66bfcd6298cbf06a2bbb6a9a2b512ead3
Wim Vanderbauwhede <wim@gpgnode-03.dcs.gla.ac.uk>
2021-09-01 15:24:13 +0100

    Various experiments to make sure it is correct.

--- tests/velfg_memory_reduction_bigger/mem_reduced_inlined/Generated/Patched/gen_velfg_superkernel.f95
+++ tests/velfg_memory_reduction_bigger/mem_reduced_inlined/Generated/Patched/gen_velfg_superkernel.f95
@@ -1,4 +1,4 @@
-
+#define MM (WM*WM)
 program main
 #ifdef WITH_OPENMP    
 use omp_lib
@@ -7,19 +7,19 @@ use singleton_module_velfg_superkernel, only : velfg_superkernel
 ! Grouped Parameter Declarations
       implicit none
       integer, parameter :: st_stage_kernel_1=1
-      integer, parameter :: niters=100
+      integer, parameter :: niters=10
 ! BEGIN new declarations
 integer :: clock_rate
 integer (kind=4), dimension(0:1) :: timestamp 
 ! END new declarations
-    integer :: global_id
+    integer*8 :: global_id
 !    ! Declarations
-    real, dimension(1:2139552*4) :: u_0
-    real, dimension(1:2139552*4) :: v_0
-    real, dimension(1:2162808*4) :: w_0
-    real, dimension(1:2074891*4) :: f_1
-    real, dimension(1:2074891*4) :: g_1
-    real, dimension(1:2074891*4) :: h_1
+    real, dimension(1:2139552*MM) :: u_0
+    real, dimension(1:2139552*MM) :: v_0
+    real, dimension(1:2162808*MM) :: w_0
+    real, dimension(1:2074891*MM) :: f_1
+    real, dimension(1:2074891*MM) :: g_1
+    real, dimension(1:2074891*MM) :: h_1
 ! Moved param decl for st_stage_kernel_1 in main to top of code unit
     integer :: state_ptr
 ! Moved param decl for niters in main to top of code unit
@@ -38,7 +38,7 @@ call system_clock(timestamp(0), clock_rate)
 #ifdef WITH_OPENMP
 !$OMP PARALLEL DO
 #endif      
-    do global_id = 1, 2025000*4
+    do global_id = 1, 2025000*MM
       call velfg_superkernel(u_0,v_0,w_0,f_1,g_1,h_1,state_ptr,global_id)
 
     end do
@@ -48,5 +48,5 @@ call system_clock(timestamp(0), clock_rate)
     end do
 call system_clock(timestamp(1), clock_rate)
 print '(f6.3)',(timestamp(1)-timestamp(0))/ real(clock_rate)
-print *, f_1(2), g_1(3),h_1(4)    
+!print *, f_1(2), g_1(3),h_1(4)    
 end program main  
