673cf770916dc440872d3bf4187e2b364141f1f3
Robert Szafarczyk <robertszafa@gmail.com>
2021-10-13 11:17:14 +0100

    [SW2D] Fix sources in autopar compiler invocation.

--- tests/ShallowWater2D/RefactoredSources/run_autoparallel_compiler.sh
+++ tests/ShallowWater2D/RefactoredSources/run_autoparallel_compiler.sh
@@ -3,9 +3,9 @@ then
 echo "Please specify the platform: CPU, GPU or FPGA"
 exit 1
 else
-if [ ! -d "../Autopar_$1/" ] 
-then 
+if [ ! -d "../Autopar_$1/" ]
+then
 mkdir "../Autopar_$1/"
 fi
-AutoParallel-Fortran-exe ./shapiro.f95 ./dyn.f95 ./update.f95 -out ../Autopar_$1/ -main ./main.f95 -v -plat $1 -N -X NO_IO -v 
+AutoParallel-Fortran-exe ./dyn_shapiro_update.f95 -out ../Autopar_$1/ -main ./main.f95 -v -plat $1 -N -X NO_IO -v
 fi
